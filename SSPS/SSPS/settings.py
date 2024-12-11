91d32971bfc8ee945a2cb3a8d0bbd60a7897d9fb
TTD <trung.do853@hcmut.edu.vn>
2024-12-11 09:52:27 +0700

    hotfix for testing

--- SSPS/SSPS/settings.py
+++ SSPS/SSPS/settings.py
@@ -38,9 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
-    'crispy_forms',
 ]
-CRISPY_TEMPLATE_PACK = 'bootstrap5'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
